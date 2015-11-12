346243b9f489321de564a8a637f17d39baf57eff
unknown <1772655815@qq.com>
2015-11-13 00:25:21 +0800

    add Modal components and removeClass function

--- H_lib.js
+++ H_lib.js
@@ -140,27 +140,107 @@
                 }
             }
         }
+        return this;
     };
     H.removeClass = function(el,value){
+        var classes, elem, cur, clazz, j, finalValue,
+            i = 0,
+            len = el.length || 1;
 
+        if (H.isFunction( value ) ) {
+            value = value.call( this, el.className );
+        }
+
+        var proceed = typeof value === "undefined" || typeof value === "string" && value;
+        if ( proceed ) {
+            classes = ( value || "" ).match( rnotwhite ) || [];
+
+            for ( ; i < len ; i++) {
+                elem = el[i] || el ;
+                cur = elem.nodeType === 1 && ( elem.className ?
+                    ( " " + elem.className + " " ).replace( rclass," " ) : ""
+                );
+
+                if ( cur ) {
+                    j = 0;
+                    while ( (clazz = classes[j++]) ) {
+                        while ( cur.indexOf( " " + clazz + " ") >= 0 ) {
+                            cur = cur.replace( " " + clazz + " ", " " );
+                        }
+                    }
+
+                    finalValue = value ? cur.trim() : "";
+                    if ( elem.className !== finalValue ) {
+                        elem.className = finalValue;
+                    }
+                }
+            }
+        }
+        return this;
     };
     //弹出框
-    H.Modal = function () {
+    H.Modal = function (option) {
+        //定义dialog对象
+        var D = function(option){
+            this.init(option);
+        };
+        //定义dialog的方法
+        D.method('init',function(option){
+            var _this = this,
+                defaults = {
+                    width: '260',
+                    height: '240',
+                    title: '提示信息',
+                    titlePostion: 'center',
+                    content: '',
+                    closeBtn: false,
+                    closeCallback: null,
+                    ok: true,
+                    okCallback: null
+                };
+            var settings = $.extend(true, defaults, option || {});
+            this.render(settings);
+        });
+        D.method('render',function(settings){
+            var _this = this;
+            //创建dom
+            this.createDom(settings);
+            //绑定事件
+            $('#dialog-ok').on('click',function(event) {
+                settings.okCallback && settings.okCallback();
+                _this.destroy();
+            });
+        });
+        D.method('createDom',function(settings){
+            var mask = '<div class="dialog-mask" style="position: fixed;width: 100%;height: 100%;top: 0; left: 0;z-index: 1000;background: rgba(0,0,0,0.4);"></div>',
+                dialogDom = '<div id="dialog-body" class="dialog-body" style="position: fixed;width: 0;height: 0;top: 50%;left: 50%;overflow: hidden;background: #fff;z-index: 1001;border-radius: 8px;transition: all 0.4s;">'+
+                    '<h3 class="dialog-title" style="text-align: '+settings.titlePostion+'; margin: 0;padding: 5px 0;background: #ccc;color:#666;">'+settings.title+'</h3>'+
+                    '<div class="dialog-content" style="width: 96%; height: '+(settings.height-75)+'px;;padding: 2%;color: #666;overflow: auto;word-break: break-all;">'+settings.content+'</div>'+
+                    '<div class="dialog-btn-group" style="text-align: center; padding: 0;">'+
+                    '<button id="dialog-ok" class="dialog-ok" style="display: '+(settings.ok?'inline-block':'none')+';border: 0; border-radius: 3px; padding: 5px 15px; font-size: 14px; color:#666;">确定</button>'+
+                    '</div>'+
+            '</div>';
+            //append到页面
+            $('body')
+                .append(mask)
+                .append(dialogDom);
+            setTimeout(function(){
+                $('#dialog-body').css({
+                    'width': settings.width,
+                    'height': settings.height,
+                    'marginTop': '-' + settings.height/2 +'px',
+                    'marginLeft': '-' + settings.width/2 +'px'
+                });
+            },100);
+        });
+        D.method('destroy',function(){
+            $('.dialog-mask').remove();
+            $('#dialog-body').remove();
+        });
 
+        return new D(option);
     };
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
     if ( typeof noGlobal === strundefined ){
         window.H = H;
     }
