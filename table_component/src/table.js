3f181d364ffaa9bdb03f34f78a325af4cf930f43
HC <huangchun_cd@keruyun.com>
2017-05-22 18:18:20 +0800

    table refactor

---
+++ table_component/src/table.js
@@ -0,0 +1,136 @@
+(function () {
+    var NO_DATA_TIP = 'no data',
+        DEFAULT_LOADING_TEXT = 'loading...';
+
+    function Table(config) {
+        var defaultConfig = {
+            hasTableHeadInBrowser: false,
+            renderContainer: $('#table'),
+            noDataTip: NO_DATA_TIP,
+            className: '',
+            columns: [],
+            renderData: []
+        };
+        var settings = $.extend({}, defaultConfig, config || {});
+        _basicCheck(settings);
+
+        this.hasTableHeadInBrowser = settings.hasTableHeadInBrowser;
+        this.renderContainer = settings.renderContainer;
+        this.className = settings.className;
+        this.noDataTip = settings.noDataTip;
+        this._columns = settings.columns;
+        this._renderData = settings.renderData;
+        this.render();
+    }
+
+    function _basicCheck(settings) {
+        var renderContainerNodeName = settings.renderContainer[0].nodeName;
+        var renderContainerIsTable = renderContainerNodeName === 'TABLE';
+        if (settings.hasTableHeadInBrowser && !renderContainerIsTable) {
+            _throwException('| error: render container should be a table, when thead is existed in browser');
+        }
+    }
+
+    function _generateTheadDomStr(columns, hasTableHeadInBrowser) {
+        if (hasTableHeadInBrowser) return '';
+        var arr = [];
+        var thStr = columns.map(function (col) {
+            return '<th>' + col.title + '</th>';
+        }).join('');
+        var trStr = thStr ? '<tr>' + thStr + '</tr>' : '';
+        arr.push('<thead>', trStr , '</thead>');
+        return arr.join('');
+    }
+
+    function _generateTbodyDomStr(columns, renderData, noDataTip) {
+        var arr = [];
+        var hasColumns = columns.length;
+        var trStr = renderData.map(function (row) {
+            var tdArr = columns.map(function (col) {
+                var tdData = row[col.dataIndex] || '';
+                if (col.render) {
+                    tdData = col.render(tdData, row);
+                    !tdData && _throwException(col.dataIndex + '| error: render can not return anything false');
+                }
+                return '<td>' + tdData + '</td>';
+            });
+            return '<tr>' + tdArr.join('') + '</tr>';
+        }).join('');
+        if (!trStr && hasColumns) {
+            trStr = '<tr><td colspan="'+ columns.length + '">' + noDataTip + '</td></tr>';
+        }
+        arr.push('<tbody>', trStr, '</tbody>');
+        return arr.join('');
+    }
+
+    function _throwException(errorMessage) {
+        throw errorMessage;
+    }
+
+    Table.prototype.getTableDom = function () {
+        var tableHeadDomStr = _generateTheadDomStr(this._columns, this.hasTableHeadInBrowser);
+        var tableBodyDomStr = _generateTbodyDomStr(this._columns, this._renderData, this.noDataTip);
+        if (this.hasTableHeadInBrowser) return $(tableBodyDomStr);
+        var className = this.className;
+        var hasClassName = !!className;
+        var $table = $('<table></table>');
+        hasClassName && $table.attr('class', className);
+
+        $table.append(tableHeadDomStr, tableBodyDomStr);
+        return $table;
+    };
+
+    Table.prototype.getMaskDom = function () {
+        return $('<div class="table-mask"><div class="loading">' + DEFAULT_LOADING_TEXT + '</div></div>');
+    };
+
+    Table.prototype.refresh = function (updatedData) {
+        this.setTableAttribute('_renderData', updatedData.renderData);
+        var newTbodyDomStr = _generateTbodyDomStr(this._columns, this._renderData);
+        this.renderContainer.find('tbody').replaceWith(newTbodyDomStr);
+        return this;
+    };
+
+    Table.prototype.setTableAttribute = function (targetAttr, newValue) {
+        this[targetAttr] = newValue;
+        return this;
+    };
+
+    Table.prototype.showLoading = function (text) {
+        var parent = this.hasTableHeadInBrowser ? this.renderContainer.parent() : this.renderContainer;
+        parent
+            .find('.table-mask')
+            .removeClass('table-mask--fadeOut')
+            .addClass('table-mask--fadeIn')
+            .fadeIn()
+            .children('.loading')
+            .html(text || DEFAULT_LOADING_TEXT);
+        return this;
+    };
+
+    Table.prototype.hideLoading = function () {
+        var parent = this.hasTableHeadInBrowser ? this.renderContainer.parent() : this.renderContainer;
+        parent
+            .find('.table-mask')
+            .removeClass('table-mask--fadeIn')
+            .addClass('table-mask--fadeOut')
+            .fadeOut();
+        return this;
+    };
+
+    Table.prototype.render = function () {
+        var tableDom = this.getTableDom();
+        var loadingMask = this.getMaskDom();
+        if (this.hasTableHeadInBrowser) {
+            this.renderContainer
+                .append(tableDom)
+                .parent()
+                .append(loadingMask);
+        } else {
+            this.renderContainer.html('').append(tableDom, loadingMask);
+        }
+        return this;
+    };
+
+    window.Table = Table;
+}());
---
+++ table_component/test/table.test.js
@@ -0,0 +1,236 @@
+var expect = chai.expect;
+describe('table component unit test', function () {
+    afterEach(function() {
+       $('#table').html('');
+    });
+    it('should_generate_a_table_when_input_table_columns_and_data', function () {
+        // given
+        var config = {
+                columns: [
+                    {title: '姓名', dataIndex: 'name'},
+                    {title: '年龄', dataIndex: 'age'}
+                ],
+                renderData: [
+                    {name: 'HC', age: 18}
+                ]
+            };
+        var table = new Table(config);
+        var expectDomStr = '<table>' +
+                        '<thead>' +
+                            '<tr><th>'+config.columns[0].title+'</th>' +
+                            '<th>'+config.columns[1].title+'</th></tr></thead>' +
+                        '<tbody>' +
+                            '<tr><td>'+config.renderData[0].name+'</td>' +
+                            '<td>'+config.renderData[0].age+'</td></tr>' +
+                        '</tbody>' +
+                    '</table>';
+        // when
+        var $tableDom = table.getTableDom();
+        var actualDomStr = $tableDom[0].outerHTML;
+
+        // then
+        expect(actualDomStr).to.equal(expectDomStr);
+    });
+
+    it('should_have_class_when_give_class_name', function () {
+        // given
+        var config = {
+                className: 'table table-bordered table-dish'
+            };
+        var table = new Table(config);
+        // when
+        var $tableDom = table.getTableDom();
+        var hasClass = $tableDom.hasClass(config.className);
+        // then
+        expect(hasClass).to.be.true;
+    });
+
+    it('should_render_operate_td_with_a_button_when_give_render_function_which_return_a_button_with_name_and_age_info_text_in_renderData_item', function () {
+        // given
+        var config = {
+                columns: [
+                    {title: '姓名', dataIndex: 'name'},
+                    {title: '年龄', dataIndex: 'age'},
+                    {title: '操作', render: function (col, row) {
+                        return '<button>'+ row.name +'</button>';
+                    }}
+                ],
+                renderData: [
+                    {
+                        name: 'HC',
+                        age: 18
+                    }
+                ]
+            };
+        var table = new Table(config);
+        // when
+        var $tableDom = table.getTableDom();
+        var thOperateTitle = $tableDom.find('th').eq(2).text();
+        var $button = $tableDom.find('button');
+        var expectButtonText = config.renderData[0].name;
+        // then
+        expect(thOperateTitle).to.equal(config.columns[2].title, 'th text should be 操作');
+        expect($button.text()).to.equal(expectButtonText, 'button text should be HC');
+    });
+
+    it('should_throw_error_when_render_function_return_not_truly', function () {
+        // given
+        var config = {
+                columns: [
+                    {
+                        title: '姓名',
+                        dataIndex: 'name',
+                        render: function() {
+                            return '';
+                        }
+                    }
+                ],
+                renderData: [
+                    {name: 'hc'}
+                ]
+            };
+        // when
+        var expectError = config.columns[0].dataIndex + '| error: render can not return anything false';
+        // then
+        expect(function() {new Table(config)}).to.throw(expectError);
+    });
+
+    it('should_throw_error_that_render_container_should_be_a_table_when_table_head_is_exist_in_browser', function () {
+        // given
+        var tableContainer = $('<div></div>');
+        var config = {
+                hasTableHeadInBrowser: true,
+                renderContainer: tableContainer
+            };
+        // when
+        var expectError = '| error: render container should be a table, when thead is existed in browser';
+        var newTableInstance = function() {new Table(config);};
+        // then
+        expect(newTableInstance).to.throw(expectError);
+    });
+
+    it('should_just_render_table_body_when_table_head_is_existed_in_browser', function () {
+        // given
+        var tableContainer = $('<table><thead></thead></table>');
+        var config = {
+                renderContainer: tableContainer,
+                hasTableHeadInBrowser: true
+            };
+        var table = new Table(config);
+        // when
+        var actualDom = table.renderContainer.find('thead').next()[0].outerHTML;
+        var expectDom = '<tbody></tbody>';
+        // then
+        expect(actualDom).to.equal(expectDom);
+    });
+
+    it('should_show_one_tip_tr_in_tbody_when_renderData_is_empty', function () {
+        //given
+        var config = {
+            renderContainer: $('<table></table>'),
+            hasTableHeadInBrowser: true,
+            noDataTip: 'no data now',
+            columns: [
+                {title: 'name', dataIndex: 'name'},
+                {title: 'age', dataIndex: 'age'}
+            ]
+        };
+        var table = new Table(config);
+        // when
+        var actualDom = table.getTableDom()[0].outerHTML;
+        var expectDom = '<tbody><tr><td colspan="2">no data now</td></tr></tbody>';
+        // then
+        expect(actualDom).to.equal(expectDom);
+    });
+
+    it('should_refresh_table_when_give_new_render_data', function () {
+        // given
+        var config = {
+            renderContainer: $('#table'),
+            columns: [
+                {title: 'name', dataIndex: 'name'}
+            ]
+        };
+        var updateData = {renderData: [{name: 'hc'}]};
+        // when
+        var table = new Table(config);
+        var updatedTable = table.refresh(updateData);
+        var expectDom = '<table>' +
+            '<thead><tr><th>name</th></tr></thead>' +
+            '<tbody><tr><td>hc</td></tr></tbody>' +
+            '</table>';
+        var actualDom = updatedTable.renderContainer.find('table')[0].outerHTML;
+        // then
+        expect(actualDom).to.equal(expectDom);
+    });
+
+
+    it('should_render_a_table_dom_and_a_table_mask_when_involve_table_render', function () {
+        // given
+        var config = {
+            renderContainer: $('#table')
+        };
+        var table = new Table(config);
+        table.render();
+        // when
+        var actualDom = table.renderContainer.html();
+        var expectDom = '<table><thead></thead><tbody></tbody></table>' +
+            '<div class="table-mask"><div class="loading">loading...</div></div>';
+        // then
+        expect(actualDom).to.equal(expectDom);
+    });
+
+    it('should_display_load_mask_when_set_table_load_show', function () {
+        // given
+        var config = {};
+        var table = new Table(config);
+        // when
+        table.showLoading();
+        var loadMask = table.renderContainer.find('.table-mask');
+        // then
+        expect(loadMask.hasClass('table-mask--fadeIn')).to.be.ok;
+    });
+
+    it('should_hide_load_mask_when_set_table_load_hide', function () {
+        // given
+        var config = {};
+        var table = new Table(config);
+        // when
+        table.hideLoading();
+        var loadMask = table.renderContainer.find('.table-mask');
+        // then
+        expect(loadMask.hasClass('table-mask--fadeOut')).to.be.ok;
+    });
+
+    it('should_display_load_mask_when_set_table_load_show_and_has_table_head', function () {
+        // given
+        $('#table').append('<table></table>');
+        var config = {
+            renderContainer: $('#table').children('table'),
+            hasTableHeadInBrowser: true
+        };
+        var table = new Table(config);
+        // when
+        table.showLoading();
+        var loadMask = table.renderContainer.parent().find('.table-mask');
+        // then
+        expect(loadMask.hasClass('table-mask--fadeIn')).to.be.ok;
+    });
+
+    it('should_hide_load_mask_when_set_table_load_hide_and_has_table_head', function () {
+        // given
+        $('#table').append('<table></table>');
+        var config = {
+            renderContainer: $('#table').children('table'),
+            hasTableHeadInBrowser: true
+        };
+        var table = new Table(config);
+        // when
+        table.hideLoading();
+        var loadMask = table.renderContainer.parent().find('.table-mask');
+        // then
+        expect(loadMask.hasClass('table-mask--fadeOut')).to.be.ok;
+    });
+
+    it('')
+});
